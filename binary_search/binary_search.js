60664f4791052d8e069f2acbad6d77492cd2e6ae
김동균 <wooheuy5752@nate.com>
2024-05-08 16:03:46 +0900

    Feat : 이진 탐색 구현

---
+++ binary_search/binary_search.js
@@ -0,0 +1,13 @@
+const binary_search = (arr, target) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (arr[middle] < target) start = middle + 1;
+    else if (arr[middle] > target) end = middle - 1;
+    else return middle;
+  }
+
+  return -1;
+};
